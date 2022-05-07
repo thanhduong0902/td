6081cbb1ffb334edf3f0349219f3173c31f727b3
unknown <thanhpot88@gmail.com>
2022-05-07 10:02:06 +0700

    1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import first from './components/first';
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/first.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function first() {
+    return (
+        <h1>Building beautiful</h1>
+    )
+}
+export default first;
